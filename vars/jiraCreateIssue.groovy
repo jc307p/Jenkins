29db2a7c647546b8452bcb59109e00308144eeeb
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-10 12:06:33 -0700

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -7,6 +7,5 @@ def call(Map config=[:]) {
     issueTypeName: "${config.issueTypeName}"
   ]
   def render = renderTemplate(rawBody,binding)
-  echo "Username: $USERNAME"
- 
+  curl -D- -u ${JIRA_CREDENTIALS} -X POST --data "'+render+'" -H "Content-Type: application/json" ${JIRA_URL}/rest/api/2/issue
 }
