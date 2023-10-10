b0a53958b6f357ea83fa37209dafc6808b5f2cd9
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-10 14:40:01 -0700

    Update jiraCreateIssue.groovy

--- vars/jiraCreateIssue.groovy
+++ vars/jiraCreateIssue.groovy
@@ -8,6 +8,6 @@ def call(Map config=[:]) {
   ]
   def render = renderTemplate(rawBody,binding)
   echo "${render}"
-  bat('curl -D- -u $JIRA_CREDENTIALS -X POST --data "{\"fields\": {\"project\":{\"key\": \"DAIOT\"},\"summary\": \"Issue Created from Jenkins\",\"description\": \"This issue is created from Jenkins\",\"issuetype\": {\"name\": \"DA IOT Defect\"}}}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
+  bat('curl -D- -u TEST_Jenkins:test1234 -X POST --data "{\"fields\": {\"project\":{\"key\": \"DAIOT\"},\"summary\": \"Issue Created from Jenkins\",\"description\": \"This issue is created from Jenkins\",\"issuetype\": {\"name\": \"DA IOT Defect\"}}}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue')
 
 }
