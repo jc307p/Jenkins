43bed844c3308365e3df31d254379f802b1fd097
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-18 14:45:06 -0700

    Create parseAndRun.groovy

---
+++ vars/parseAndRun.groovy
@@ -0,0 +1,11 @@
+def parseAndRun(String A) {
+    def A_dict = new groovy.json.JsonSlurper().parseText("{${A}")
+
+    def script(Map<String, String> kwargs) {
+        kwargs.each { key, value ->
+            println "${key}: ${value}"
+        }
+    }
+
+    script(A_dict)
+}
