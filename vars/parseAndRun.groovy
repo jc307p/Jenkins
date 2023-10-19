505978f31e57270de0841c4c2e2e37363c19f077
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-19 07:46:24 -0700

    Update parseAndRun.groovy

--- vars/parseAndRun.groovy
+++ vars/parseAndRun.groovy
@@ -1,3 +1,4 @@
 def parseAndRun(String A) {
-    def A_dict = new groovy.json.JsonSlurper().parseText("{${A}")
+    def A_dict = new groovy.json.JsonSlurper().parseText("${A}")
+    echo "${A_dict}"
 }
