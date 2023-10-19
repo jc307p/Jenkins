ab70c3c79755b73d0e3358718b1ba91dc55b927a
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-19 15:52:49 -0700

    Create WebhookTriggerJiraConfig.groovy

---
+++ vars/WebhookTriggerJiraConfig.groovy
@@ -0,0 +1,15 @@
+triggersConfig = [
+  [
+    genericVariables:[
+      [key: "Variable_Name", value: "JSONPath"]
+    ],
+    token:"your-token",
+    causeString:"triggered from Jira",
+    printContributedVariables: false,
+    printPostContent: false,
+    silentResponse: false,
+    regexFilterText:"",
+    regexFilterExpression:""
+  ]
+]
+    
