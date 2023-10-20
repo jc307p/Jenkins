1a18f1585fde3d18764db056c72911c553750b0d
jc307p <147084581+jc307p@users.noreply.github.com>
2023-10-20 11:32:12 -0700

    Update genericWebhookTriggers.groovy

--- vars/genericWebhookTriggers.groovy
+++ vars/genericWebhookTriggers.groovy
@@ -1,26 +1,12 @@
 import java.nio.file.Paths
 import java.util.Properties
 
-def getConfig(String configFile) {
-  def configPath = Paths.get(".", "com", "planetpope", "config", configFile)
-  def config = configPath.toFile()
-  echo "config is ${config}"
-  
-  if (config.exists()) {
-    config.withReader { reader ->
-      new GroovyShell().evaluate(reader)
-    }
-  } else {
-      error('Configuration file "${configFile}" not found')
-  }
-}
-
 def call(String configFile){
-  echo "configFile is ${configFile}"
+//  echo "configFile is ${configFile}"
   def basePath = 'com/planetpope/config/'
   def fullPath = basePath + configFile
   def configContent = libraryResource fullPath
-  echo "configContent is ${configContent}"
+//  echo "configContent is ${configContent}"
 //  def configClass = config.getClass()
 //  echo "config class is ${configClass}"
 
@@ -28,16 +14,16 @@ def call(String configFile){
   config.load(new StringReader(configContent))
 
   String causeString = config.getProperty("causeString")
-  echo "causeString is ${causeString}"
+//  echo "causeString is ${causeString}"
 
 //  triggersConfig.each {trigger ->
-//    properties([
-//      pipelineTriggers([
-//        [
-//          $class:'GenericTrigger',
+    properties([
+      pipelineTriggers([
+        [
+          $class:'GenericTrigger',
 //          genericVariables: trigger.genericVariables,
 //          token: trigger.token,
-//          causeString: trigger.causeString,
+          causeString: "${causeString}",
 //          printContributedVariables: trigger.printContributedVariables,
 //          printPostContent: trigger.printPostContent,
 //          silentResponse: trigger.silentResponse,
